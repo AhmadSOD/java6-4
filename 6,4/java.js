289a9910ccd39962724a463358760678d8038069
AhmadSOD <144031243+AhmadSOD@users.noreply.github.com>
2023-10-11 14:21:29 +0200

    Add files via upload

---
+++ 6,4/java.js
@@ -0,0 +1,20 @@
+function calculate(getal1, getal2)
+{
+  antwoord = getal1 + getal2;
+  console.log("het antwoord van de som is: ");
+  console.log(antwoord);
+}
+
+function keer5(getal1)
+{
+  antwoord = getal1 * 5;
+  console.log("het antwoord van de som is: ");
+  console.log(antwoord);
+}
+
+function calculateMinutes(seconden)
+{
+antwoord = seconden / 60;
+console.log("het antwoord van de som is: ");
+console.log(antwoord);
+}
